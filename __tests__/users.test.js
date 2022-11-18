b5b71d33a22b1158df15661378ca2993622e3774
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-11-18 10:39:38 -0800

    first user test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,14 +1,24 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const userTest = {
+  email: 'test@test.com',
+  password: '123456',
+};
+
+describe('/api/v1/users route', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  test('/api/v1/users route', async () => {
+    const res = await request(app).post('/api/v1/users').send(userTest);
+    const { email } = userTest;
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
   afterAll(() => {
     pool.end();
