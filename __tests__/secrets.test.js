7be32088b06844af9b3ed6688208ca3d7752b552
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-11-29 10:59:01 -0800

    secrets setup

---
+++ __tests__/secrets.test.js
@@ -0,0 +1,38 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? testUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...testUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({
+    email,
+    password,
+  });
+  return [agent, user];
+};
+
+const testUser = {
+  email: 'test@test.com',
+  password: '1234password',
+};
+
+describe('/api/v1/secrets routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  test('displays list of secrets to authenticated users only', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/v1/secrets');
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(2);
+  });
+});
+afterAll(() => {
+  pool.end();
+});
